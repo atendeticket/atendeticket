3e710cdb94d55366ee3752295cbc1da3f0480bb7
lipedevv <lipedevv@users.noreply.github.com>
2025-12-09 20:39:46 -0300

    AAAAAAAAAAA

--- installers/system/installNode.js
+++ installers/system/installNode.js
@@ -3,7 +3,7 @@ const { info, success } = require('../../core/logger');
 
 async function installNode() {
     info('Instalando Node.js + NPM + NPX...');
-    await runCommand('curl', ['-fsSL', 'https://deb.nodesource.com/setup_20.x', '|', 'bash', '-']);
+    await runCommand('bash', ['-c', 'curl -fsSL https://deb.nodesource.com/setup_20.x | bash -']);
     await runCommand('sudo', ['apt', 'install', '-y', 'nodejs']);
     await runCommand('sudo', ['npm', 'install', '-g', 'npm@latest']);
     success('Node.js + NPM + NPX instalados!');
